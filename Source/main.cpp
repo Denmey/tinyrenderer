59d6fe6ca6e037b17d4ca16dbf4d6165be3cdc55
Denmey <Denmey98@gmail.com>
2019-03-13 18:57:34 +0300

    Fixed errors

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,12 +2,12 @@
 #define IMAGE_WIDTH 100
 #define IMAGE_HEIGHT 100
 
-const TGAColor white = (255, 255, 255, 255);
-const TGAColor   red = (255, 0,   0,   255);
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor   red = TGAColor(255, 0,   0,   255);
 
 int main(int argc, char const *argv[]) {
-	TGAimage image(100, 100, TGAImage::RGB);
-	image.set(52, 41, red);
+	TGAImage image(100, 100, TGAImage::RGB);
+	image.set(52, 41, white);
 	image.flip_vertically(); // Origin is at left-bottom
 	image.write_tga_file("output.tga");
 	return 0;
