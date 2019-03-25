1b2cf9c69c517fd8eab8a31418aad05326f60466
Denmey <Denmey98@gmail.com>
2019-03-26 00:35:34 +0300

    Added triangles

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,23 +1,53 @@
 #include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
 #include <iostream>
-#define IMAGE_WIDTH 100
-#define IMAGE_HEIGHT 100
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor   red = TGAColor(255, 0,   0,   255);
+const TGAColor green = TGAColor(0,   255, 0,   255);
+const TGAColor  blue = TGAColor(0,   0,   255, 255);
+Model *model = NULL;
+const int width  = 200;
+const int height = 200;
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
+void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &color);
+void triangle(Vec2i p1, Vec2i p2, Vec2i p3, TGAImage &image, const TGAColor &color);
+void filledTriangle(Vec2i p1, Vec2i p2, Vec2i p3, TGAImage &image, const TGAColor &color);
 
 int main(int argc, char const *argv[]) {
-	TGAImage image(100, 100, TGAImage::RGB);
-	image.set(52, 41, white);
-	line(0, 0, 11,  100, image, red);
-	line(22, 100, 0, 0, image, white);
-	line(0, 0, 100, 11,  image, white);
-	line(100, 22, 0, 0,  image, red);
-	line(0,0,0,100, image, white);
-	line(0,0,100,0, image, white);
-	line(20,20,20,20,image,red);
+	TGAImage image(width, height, TGAImage::RGB);
+	// if (2==argc) {
+	// 	model = new Model(argv[1]);
+	// } else {
+	// 	model = new Model("../obj/african_head.obj");
+	// }
+	// for (int i = 0; i < 1000000; ++i)
+	// 	line(0, 0, 11,  100, image, red);
+	// for (int i=0; i<model->nfaces(); i++) {
+	// 	std::vector<int> face = model->face(i);
+	// 	for (int j=0; j<3; j++) {
+	// 		Vec3f v0 = model->vert(face[j]);
+	// 		Vec3f v1 = model->vert(face[(j+1)%3]);
+	// 		int x0 = (v0.x+1.)*width/2.;
+	// 		int y0 = (v0.y+1.)*height/2.;
+	// 		int x1 = (v1.x+1.)*width/2.;
+	// 		int y1 = (v1.y+1.)*height/2.;
+	// 		line(x0, y0, x1, y1, image, white);
+	// 	}
+	// }
+
+	Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
+	Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
+	Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+	triangle(t0[0], t0[1], t0[2], image, white);
+	triangle(t1[0], t1[1], t1[2], image, red);
+	triangle(t2[0], t2[1], t2[2], image, white);
+	filledTriangle(t0[0], t0[1], t0[2], image, red);
+	filledTriangle(t1[0], t1[1], t1[2], image, white);
+	filledTriangle(t2[0], t2[1], t2[2], image, green);
+
+
 	image.flip_vertically(); // Origin is at left-bottom
 	image.write_tga_file("output.tga");
 	return 0;
@@ -27,7 +57,7 @@ int sign(int x) {
 	return (x<0) ? -1 : 1;
 }
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &color) {
 	int dx = x1-x0;
 	int dy = y1-y0;
 	if (dx == 0 && dy == 0)
@@ -35,11 +65,47 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 	if (std::abs(dx) >= std::abs(dy)) {
 		float diff = std::abs(1.0f*dy/dx);
 		for (int i = 0; i <= std::abs(dx); ++i)
-			image.set(x0+sign(dx)*i, y0+sign(dy)*i*diff, color);
+			image.set(x0+sign(dx)*i, y0+std::round(sign(dy)*i*diff), color);
 	}
 	else {
 		float diff = std::abs(1.0f*dx/dy);
 		for (int i = 0; i <= std::abs(dy); ++i)
-			image.set(x0+sign(dx)*i*diff, y0+sign(dy)*i, color);
+			image.set(x0+std::round(sign(dx)*i*diff), y0+sign(dy)*i, color);
 	}
+}
+
+void triangle(Vec2i p1, Vec2i p2, Vec2i p3, TGAImage &image, const TGAColor &color) {
+	if (p1.x > p2.x) std::swap(p1, p2);
+	if (p2.x > p3.x) std::swap(p2, p3);
+	if (p1.x > p3.x) std::swap(p1, p3);
+	line(p1.x, p1.y, p2.x, p2.y, image, color);
+	line(p2.x, p2.y, p3.x, p3.y, image, color);
+	line(p3.x, p3.y, p1.x, p1.y, image, color);
+}
+
+void filledTriangle(Vec2i p1, Vec2i p2, Vec2i p3, TGAImage &image, const TGAColor &color) {
+	if (p1.x > p2.x) std::swap(p1, p2);
+	if (p2.x > p3.x) std::swap(p2, p3);
+	if (p1.x > p2.x) std::swap(p1, p2);
+	int   x12  = std::abs(p2.x-p1.x),
+	      x23  = std::abs(p3.x-p2.x),
+	      x13  = x12 + x23;
+	float currx  = p1.x,
+	      curry1 = p1.y,
+	      curry2 = p1.y,
+	      dy12   = float(p2.y-p1.y)/x12,
+	      dy23   = float(p3.y-p2.y)/x23,
+	      dy13   = float(p3.y-p1.y)/x13;
+	std::cout << dy12 << " " << dy13 << " " << dy23 << '\n';
+	for(; currx < p2.x; ++currx) {
+		line(currx, curry1, currx, curry2, image, color);
+		curry1 += dy12;
+		curry2 += dy13;
+	}
+	for(; currx <= p3.x; ++currx) {
+		line(currx, curry1, currx, curry2, image, color);
+		curry1 += dy23;
+		curry2 += dy13;
+	}
+	line(p1.x, p1.y, p1.x, p1.y, image, TGAColor(255, 255, 0, 255));
 }
